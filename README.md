0f6bb5f6ee10a15debe8c3af8d4756918b25fa5a
GermanBluefox <dogafox@gmail.com>
2017-10-06 18:25:57 +0200

    ### 2.1.5 (2017-10-06) * (bluefox) Fix link to local admin on welcome screen

--- README.md
+++ README.md
@@ -30,6 +30,9 @@ If authentication is enabled and the user enters 5 times invalid password during
 After 15th wrong attempt the user mast wait 1 hour.
 
 ## Changelog
+### 2.1.5 (2017-10-06)
+* (bluefox) Fix link to local admin on welcome screen
+
 ### 2.1.4 (2017-10-02)
 * (bluefox) Support of iobroker.pro
 
--- main.js
+++ main.js
@@ -204,6 +204,134 @@ var specialScreen = [
 
 var indexHtml;
 
+function getLinkVar(_var, obj, attr, link) {
+    if (attr === 'protocol') attr = 'secure';
+
+    if (_var === 'ip') {
+        link = link.replace('%' + _var + '%', '$host$');
+    } else
+    if (_var === 'instance') {
+        var instance = obj._id.split('.').pop();
+        link = link.replace('%' + _var + '%', instance);
+    } else {
+        if (obj) {
+            if (attr.match(/^native_/)) attr = attr.substring(7);
+
+            var val = obj.native[attr];
+            if (_var === 'bind' && (!val || val === '0.0.0.0')) val = '$host$';
+
+            if (attr === 'secure') {
+                link = link.replace('%' + _var + '%', val ? 'https' : 'http');
+            } else {
+                if (link.indexOf('%' + _var + '%') === -1) {
+                    link = link.replace('%native_' + _var + '%', val);
+                } else {
+                    link = link.replace('%' + _var + '%', val);
+                }
+            }
+        } else {
+            if (attr === 'secure') {
+                link = link.replace('%' + _var + '%', 'http');
+            } else {
+                if (link.indexOf('%' + _var + '%') === -1) {
+                    link = link.replace('%native_' + _var + '%', '');
+                } else {
+                    link = link.replace('%' + _var + '%', '');
+                }
+            }
+        }
+    }
+    return link;
+}
+
+function resolveLink(link, instanceObj, instancesMap) {
+    var vars = link.match(/%(\w+)%/g);
+    var _var;
+    var v;
+    var parts;
+    if (vars) {
+        // first replace simple patterns
+        for (v = vars.length - 1; v >= 0; v--) {
+            _var = vars[v];
+            _var = _var.replace(/%/g, '');
+
+            parts = _var.split('_');
+            // like "port"
+            if (_var.match(/^native_/)) {
+                link = getLinkVar(_var, instanceObj, _var, link);
+                vars.splice(v, 1);
+            } else
+            if (parts.length === 1) {
+                link = getLinkVar(_var, instanceObj, parts[0], link);
+                vars.splice(v, 1);
+            } else
+            // like "web.0_port"
+            if (parts[0].match(/\.[0-9]+$/)) {
+                link = getLinkVar(_var, instancesMap['system.adapter.' + parts[0]], parts[1], link);
+                vars.splice(v, 1);
+            }
+        }
+        var links = {};
+        var instances;
+        var adptr = parts[0];
+        // process web_port
+        for (v = 0; v < vars.length; v++) {
+            _var = vars[v];
+            _var = _var.replace(/%/g, '');
+            if (_var.match(/^native_/)) _var = _var.substring(7);
+
+            parts = _var.split('_');
+            if (!instances) {
+                instances = [];
+                for (var inst = 0; inst < 10; inst++) {
+                    if (that.main.objects['system.adapter.' + adptr + '.' + inst]) instances.push(inst);
+                }
+            }
+
+            for (var i = 0; i < instances.length; i++) {
+                links[adptr + '.' + i] = {
+                    instance: adptr + '.' + i,
+                    link: getLinkVar(_var, instancesMap['system.adapter.' + adptr + '.' + i], parts[1], links[adptr + '.' + i] ? links[adptr + '.' + i].link : link)
+                };
+            }
+        }
+        var result;
+        if (instances) {
+            result = [];
+            var count = 0;
+            var firtsLink = '';
+            for (var d in links) {
+                result[links[d].instance] = links[d].link;
+                if (!firtsLink) firtsLink = links[d].link;
+                count++;
+            }
+            if (count < 2) {
+                link = firtsLink;
+                result = null;
+            }
+        }
+    }
+    return result || link;
+}
+
+function replaceInLink(link, instanceObj, instances) {
+    if (typeof link === 'object') {
+        var links = JSON.parse(JSON.stringify(link));
+        var first;
+        for (var v in links) {
+            if (links.hasOwnProperty(v)) {
+                links[v] = resolveLink(links[v], instanceObj, instances);
+                if (!first) first = links[v];
+            }
+        }
+        links.__first = first;
+        return links;
+    } else {
+        return resolveLink(link, instanceObj, instances);
+    }
+}
+
+
 function getListOfAllAdapters(callback) {
     try {
         // read all instances
@@ -211,18 +339,23 @@ function getListOfAllAdapters(callback) {
             adapter.objects.getObjectView('system', 'adapter', {}, function (err, adapters) {
                 var list = [];
                 var a;
+                var mapInstance = {};
+                for (var r = 0; r < instances.rows.length; r++) {
+                    mapInstance[instances.rows[r].id] = instances.rows[r].value;
+                }
                 for (a = 0; a < adapters.rows.length; a++) {
                     var obj = adapters.rows[a].value;
-                    var found = true;
+                    var found = '';
                     if (instances && instances.rows) {
-                        found = false;
+                        found = '';
+                        // find if any instance of this adapter is exists and started
                         for (var i = 0; i < instances.rows.length; i++) {
                             var id = instances.rows[i].id;
                             var ids = id.split('.');
                             ids.pop();
                             id = ids.join('.');
-                            if (id === obj._id) {
-                                found = true;
+                            if (id === obj._id && instances.rows[i].value.common && instances.rows[i].value.common.enabled) {
+                                found = instances.rows[i].id;
                                 break;
                             }
                         }
@@ -237,9 +370,21 @@ function getListOfAllAdapters(callback) {
                                         if (obj.common.welcomeScreen[w].name === 'vis editor') {
                                             continue;
                                         }
+                                        if (obj.common.welcomeScreen[w].localLink && typeof obj.common.welcomeScreen[w].localLink === 'boolean') {
+                                            obj.common.welcomeScreen[w].localLink = obj.common.localLink;
+                                        }
+                                        if (obj.common.welcomeScreen[w].localLink) {
+                                            obj.common.welcomeScreen[w].id = found;
+                                        }
                                         list.push(obj.common.welcomeScreen[w]);
                                     }
                                 } else {
+                                    if (obj.common.welcomeScreen.localLink && typeof obj.common.welcomeScreen.localLink === 'boolean') {
+                                        obj.common.welcomeScreen.localLink = obj.common.localLink;
+                                    }
+                                    if (obj.common.welcomeScreen.localLink) {
+                                        obj.common.welcomeScreen.id = found;
+                                    }
                                     list.push(obj.common.welcomeScreen);
                                 }
                             }
@@ -248,11 +393,23 @@ function getListOfAllAdapters(callback) {
                                     for (var ww = 0; ww < obj.common.welcomeScreenPro.length; ww++) {
                                         var tile = Object.assign({}, obj.common.welcomeScreenPro[ww]);
                                         tile.pro = true;
+                                        if (tile.localLink && typeof tile.localLink === 'boolean') {
+                                            tile.localLink = obj.common.localLink;
+                                        }
+                                        if (tile.localLink) {
+                                            tile.id = found;
+                                        }
                                         list.push(tile);
                                     }
                                 } else {
                                     var tile_ = Object.assign({}, obj.common.welcomeScreenPro);
                                     tile_.pro = true;
+                                    if (tile_.localLink && typeof tile_.localLink === 'boolean') {
+                                        tile_.localLink = obj.common.localLink;
+                                    }
+                                    if (tile_.localLink) {
+                                        tile_.id = found;
+                                    }
                                     list.push(tile_);
                                 }
                             }
@@ -266,6 +423,7 @@ function getListOfAllAdapters(callback) {
                         }
                     }
                 }
+
                 indexHtml = /*indexHtml || */fs.readFileSync(__dirname + '/www/index.html').toString();
                 list.sort(function (a, b) {
                     if (a.order === undefined && b.order === undefined) {
@@ -284,6 +442,13 @@ function getListOfAllAdapters(callback) {
                         return 0;
                     }
                 });
+
+                // calculate localLinks
+                for (var t = 0; t < list.length; t++) {
+                    if (list[t].localLink) {
+                        list[t].localLink = resolveLink(list[t].localLink, mapInstance[list[t].id], mapInstance);
+                    }
+                }
 
                 var text = 'systemLang = "' + lang + '";\n';
                 text += 'list = ' + JSON.stringify(list, null, 2) + ';\n';
